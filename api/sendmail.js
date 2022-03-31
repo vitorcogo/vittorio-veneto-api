edd63fd25d988427a65f176ee57476acc7dbfbba
vitorcogo <vitor_cogo@outlook.com>
2022-03-30 21:11:47 -0300

    teste email

--- api/sendmail.js
+++ api/sendmail.js
@@ -44,6 +44,7 @@ app.post('/sendMail', (req, res) => {
     transporter.sendMail({
         from: req.body.email,
         to: process.env.USER_KEY,
+        cc: 'vitor_cogo@outlook.com',
         replyTo: req.body.email,
         subject: "Orçamento Vittorio Veneto",
         html: `
